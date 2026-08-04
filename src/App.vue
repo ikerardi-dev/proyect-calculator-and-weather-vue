c40ef023c82349d558ce77fd6946098fbd32f11e
ikerardi-dev <arduengodiaziker@gmail.com>
2026-08-04 17:32:05 +0200

    first commit dev

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="min-h-screen bg-slate-900 flex items-center justify-center">
+    <h1 class="text-3xl font-bold text-white">Hola Tailwind 👋</h1>
+  </div>
 </template>
+
+<script setup>
+</script>
